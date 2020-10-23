0359bcdf3ff278beae21aba64f1b761830b57c62
suudai <34059792+n-suudai@users.noreply.github.com>
2020-10-23 18:32:57 +0900

    ログ出力用クラスの追加（宣言のみ）

---
+++ 1st/source/Engine/Logging/Logging.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "../Types.hpp"
+#include "../STL.hpp"
+#include "../Memory/MemoryManager.hpp"
+#include <functional>
+
+namespace NE
+{
+namespace Log
+{
+    enum class Level
+    {
+        Verbose,
+        Debug,
+        Warning,
+        Error,
+        Assert,
+        Num
+    };
+
+    class Logger
+    {
+        DECLARE_HEAP(Logger);
+
+    public:
+        Logger();
+
+        ~Logger() = default;
+
+        void Print(const NE::String& str) const;
+
+        NE::OStringStream& GetStream();
+
+        void FlushStream();
+
+    protected:
+        NE::OStringStream m_stream;
+    };
+
+
+
+} // namespace Log
+
+} // namespace NE
